66b5109a014ee61407a49b585fc71942d760e0f1
Chris Patterson <chris@phatboyg.com>
2008-09-11 21:37:07 +0000

    Added function timer and some new reflection tools for fast property access to classes

---
+++ src/Magnum.Common/FunctionTimer.cs
@@ -0,0 +1,119 @@
+// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace Magnum.Common
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics;
+	using System.Text;
+
+	public class FunctionTimer : IDisposable
+	{
+		private readonly Action<string> _action;
+		private readonly string _description;
+		private readonly List<Stopwatch> _marks = new List<Stopwatch>(10);
+		private readonly Stopwatch _stopwatch;
+		private DateTime _started;
+
+		public FunctionTimer(string description, Action<string> action)
+		{
+			_description = description;
+			_action = action;
+			_started = DateTime.UtcNow;
+
+			_stopwatch = Stopwatch.StartNew();
+		}
+
+		public virtual string Header
+		{
+			get
+			{
+				StringBuilder sb = new StringBuilder(256);
+
+				sb.Append("Date Time TimeTaken");
+
+				for (int i = 0; i < _marks.Count; i++)
+				{
+					sb.Append(" Mark").Append(i + 1);
+				}
+
+				OutputAdditionalHeaderValues(sb);
+
+				if (!string.IsNullOrEmpty(_description))
+					sb.Append(" Description");
+
+				return sb.ToString();
+			}
+		}
+
+		public void Dispose()
+		{
+			_stopwatch.Stop();
+
+			_action(ToString());
+		}
+
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder(256);
+
+			sb.Append(_started.ToString("yyyy-MM-dd HH:mm:ss ")).Append(_stopwatch.ElapsedMilliseconds);
+
+			foreach (Stopwatch mark in _marks)
+			{
+				sb.Append(' ').Append(mark.ElapsedMilliseconds);
+			}
+
+			OutputAdditionalValues(sb);
+
+			if (!string.IsNullOrEmpty(_description))
+				sb.Append(" ").Append(_description);
+
+			return sb.ToString();
+		}
+
+		protected virtual void OutputAdditionalValues(StringBuilder sb)
+		{
+		}
+
+		protected virtual void OutputAdditionalHeaderValues(StringBuilder sb)
+		{
+		}
+
+		public CheckPoint Mark()
+		{
+			Stopwatch watch = Stopwatch.StartNew();
+			_marks.Add(watch);
+
+			CheckPoint point = new CheckPoint(watch);
+
+			return point;
+		}
+
+
+		public class CheckPoint : IDisposable
+		{
+			private readonly Stopwatch _stopwatch;
+
+			public CheckPoint(Stopwatch stopwatch)
+			{
+				_stopwatch = stopwatch;
+			}
+
+			public void Dispose()
+			{
+				_stopwatch.Stop();
+			}
+		}
+	}
+}
---
+++ src/Magnum.Common/Reflection/FastProperty.cs
@@ -0,0 +1,147 @@
+// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace Magnum.Common.Reflection
+{
+	using System;
+	using System.Linq.Expressions;
+	using System.Reflection;
+
+	public class FastProperty
+	{
+		public Func<object, object> GetDelegate;
+		public Action<object, object> SetDelegate;
+
+		public FastProperty(PropertyInfo property)
+		{
+			Property = property;
+			InitializeGet();
+			InitializeSet();
+		}
+
+		public PropertyInfo Property { get; set; }
+
+		private void InitializeSet()
+		{
+			var instance = Expression.Parameter(typeof (object), "instance");
+			var value = Expression.Parameter(typeof (object), "value");
+
+			// value as T is slightly faster than (T)value, so if it's not a value type, use that
+			UnaryExpression instanceCast = (!Property.DeclaringType.IsValueType) ?
+				Expression.TypeAs(instance, Property.DeclaringType) : Expression.Convert(instance, Property.DeclaringType);
+			UnaryExpression valueCast = (!Property.PropertyType.IsValueType) ? 
+				Expression.TypeAs(value, Property.PropertyType) : Expression.Convert(value, Property.PropertyType);
+
+			SetDelegate = Expression.Lambda<Action<object, object>>(Expression.Call(instanceCast, Property.GetSetMethod(), valueCast), new[] {instance, value}).Compile();
+		}
+
+		private void InitializeGet()
+		{
+			var instance = Expression.Parameter(typeof (object), "instance");
+			UnaryExpression instanceCast = (!Property.DeclaringType.IsValueType) ?
+				Expression.TypeAs(instance, Property.DeclaringType) : Expression.Convert(instance, Property.DeclaringType);
+
+			GetDelegate = Expression.Lambda<Func<object, object>>(Expression.TypeAs(Expression.Call(instanceCast, Property.GetGetMethod()), typeof (object)), instance).Compile();
+		}
+
+		public object Get(object instance)
+		{
+			return GetDelegate(instance);
+		}
+
+		public void Set(object instance, object value)
+		{
+			SetDelegate(instance, value);
+		}
+	}
+
+	public class FastProperty<T>
+	{
+		public Func<T, object> GetDelegate;
+		public Action<T, object> SetDelegate;
+
+		public FastProperty(PropertyInfo property)
+		{
+			Property = property;
+			InitializeGet();
+			InitializeSet();
+		}
+
+		public PropertyInfo Property { get; set; }
+
+		private void InitializeSet()
+		{
+			var instance = Expression.Parameter(typeof (T), "instance");
+			var value = Expression.Parameter(typeof (object), "value");
+			UnaryExpression valueCast = (!Property.PropertyType.IsValueType) ? Expression.TypeAs(value, Property.PropertyType) : Expression.Convert(value, Property.PropertyType);
+			SetDelegate = Expression.Lambda<Action<T, object>>(Expression.Call(instance, Property.GetSetMethod(), valueCast), new[] {instance, value}).Compile();
+		}
+
+		private void InitializeGet()
+		{
+			var instance = Expression.Parameter(typeof (T), "instance");
+			GetDelegate = Expression.Lambda<Func<T, object>>(Expression.TypeAs(Expression.Call(instance, Property.GetGetMethod()), typeof (object)), instance).Compile();
+		}
+
+		public object Get(T instance)
+		{
+			return GetDelegate(instance);
+		}
+
+		public void Set(T instance, object value)
+		{
+			SetDelegate(instance, value);
+		}
+	}
+
+	public class FastProperty<T, P>
+	{
+		public Func<T, P> GetDelegate;
+		public Action<T, P> SetDelegate;
+
+		public FastProperty(PropertyInfo property)
+		{
+			Property = property;
+			InitializeGet();
+			InitializeSet();
+		}
+
+		public PropertyInfo Property { get; set; }
+
+		private void InitializeSet()
+		{
+			var instance = Expression.Parameter(typeof (T), "instance");
+			var value = Expression.Parameter(typeof (P), "value");
+			SetDelegate = Expression.Lambda<Action<T, P>>(Expression.Call(instance, Property.GetSetMethod(), value), new[] {instance, value}).Compile();
+
+			// roughly looks like Action<T,P> a = new Action<T,P>((instance,value) => instance.set_Property(value));
+		}
+
+		private void InitializeGet()
+		{
+			var instance = Expression.Parameter(typeof (T), "instance");
+			GetDelegate = Expression.Lambda<Func<T, P>>(Expression.Call(instance, Property.GetGetMethod()), instance).Compile();
+
+			// roughly looks like Func<T,P> getter = instance => return instance.get_Property();
+		}
+
+		public P Get(T instance)
+		{
+			return GetDelegate(instance);
+		}
+
+		public void Set(T instance, P value)
+		{
+			SetDelegate(instance, value);
+		}
+	}
+}
