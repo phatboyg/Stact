d2b7e0d11a449b2ad0143ed11fd271d1b3a21e60
Dru Sellers <dru@drusellers.com>
2008-09-12 23:02:55 +0000

    Order of properties is important

--- src/Magnum.Common.Tests/Reflection/InterOp_Specs.cs
+++ src/Magnum.Common.Tests/Reflection/InterOp_Specs.cs
@@ -1,34 +1,32 @@
 namespace Magnum.Common.Tests.Reflection
 {
-    using System.Diagnostics;
     using NUnit.Framework;
 
     [TestFixture]
     public class InterOp_Specs :
         SerializationSpecificationBase
     {
-        private readonly Bob bob = new Bob{Name="Dru"};
-
-        
+        private readonly Bob _bob = new Bob { Name = "Dru" };
+        private readonly Order1 _order = new Order1{FirstName = "Chris", LastName = "Patterson"};
 
         [Test]
         public void Shouldnt_care_about_namespaces()
         {
             byte[] bytes;
-            bytes = Serialize(bob);
+            bytes = Serialize(_bob);
             var output = Deserialize<Different.Bob>(bytes);
 
-            Assert.AreEqual(bob.Name, output.Name);
+            Assert.AreEqual(_bob.Name, output.Name);
         }
 
         [Test]
         public void Shouldnt_care_about_class_names()
         {
             byte[] bytes;
-            bytes = Serialize(bob);
+            bytes = Serialize(_bob);
             var output = Deserialize<Bill>(bytes);
 
-            Assert.AreEqual(bob.Name, output.Name);
+            Assert.AreEqual(_bob.Name, output.Name);
         }
 
 
@@ -36,28 +34,51 @@ namespace Magnum.Common.Tests.Reflection
         public void Shouldnt_care_about_casing()
         {
             byte[] bytes;
-            bytes = Serialize(bob);
+            bytes = Serialize(_bob);
             var output = Deserialize<Jack>(bytes);
 
-            Assert.AreEqual(bob.Name, output.name);
+            Assert.AreEqual(_bob.Name, output.name);
         }
-    }
 
-    public class Bob
-    {
-        public string Name { get; set; }
-    }
+        [Test]
+        public void Shouldnt_care_about_order()
+        {
+            byte[] bytes;
+            bytes = Serialize(_order);
+            var output = Deserialize<Order2>(bytes);
 
-    public class Bill
-    {
-        public string Name { get; set; }
-    }
+            Assert.AreEqual(_order.FirstName, output.FirstName);
+            Assert.AreEqual(_order.LastName, output.LastName);
+        }
 
-    public class Jack
-    {
-        public string name { get; set; }
-    }
+        public class Bob
+        {
+            public string Name { get; set; }
+        }
 
+        public class Bill
+        {
+            public string Name { get; set; }
+        }
+
+        public class Jack
+        {
+            public string name { get; set; }
+        }
+
+        public class Order1
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        public class Order2
+        {
+            public string LastName { get; set; }
+            public string FirstName { get; set; }
+        }
+
+    }
     namespace Different
     {
         public class Bob
@@ -65,4 +86,5 @@ namespace Magnum.Common.Tests.Reflection
             public string Name { get; set; }
         }
     }
+
 }
