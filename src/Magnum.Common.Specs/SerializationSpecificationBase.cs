0367becd90d3617f7102553fdc7d21b2fe352d51
Chris Patterson <chris@phatboyg.com>
2008-09-13 01:48:16 +0000

    No need to loop through 10,000 times for a test base :)

--- src/Magnum.Common.Specs/SerializationSpecificationBase.cs
+++ src/Magnum.Common.Specs/SerializationSpecificationBase.cs
@@ -1,3 +1,15 @@
+// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
 namespace Magnum.Common.Specs
 {
     using System.IO;
@@ -36,17 +48,10 @@ namespace Magnum.Common.Specs
             using (var binaryReader = new BinaryReader(output))
             {
                 var writer = new BinarySerializationReader(binaryReader);
+                output.Seek(0, SeekOrigin.Begin);
 
-
-                for (int i = 0; i < 300000; i++)
-                {
-                    output.Seek(0, SeekOrigin.Begin);
-
-                    obj = (T) FormatterServices.GetUninitializedObject(typeof (T));
-                    SerializationUtil<T>.Deserialize(writer, obj);
-
-                }
-
+                obj = (T) FormatterServices.GetUninitializedObject(typeof (T));
+                SerializationUtil<T>.Deserialize(writer, obj);
             }
 
             return obj;
