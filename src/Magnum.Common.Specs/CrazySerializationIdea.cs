481de6ef60f05d9bd00d06492ba06011d987a0b8
Chris Patterson <chris@phatboyg.com>
2008-11-05 05:01:18 +0000

    Working ideas on protocol buffer spec

--- src/Magnum.Common.Specs/CrazySerializationIdea.cs
+++ src/Magnum.Common.Specs/CrazySerializationIdea.cs
@@ -15,6 +15,8 @@ namespace Magnum.Common.Specs
     using System;
     using System.Collections.Generic;
     using System.Linq.Expressions;
+    using System.Reflection;
+    using System.Runtime.Serialization;
     using NUnit.Framework;
 
     [TestFixture]
@@ -24,6 +26,8 @@ namespace Magnum.Common.Specs
         public void FIRST_TEST_NAME()
         {
             CrazyMesssageMap map = new CrazyMesssageMap();
+
+            map.Validate();
         }
     }
 
@@ -32,9 +36,11 @@ namespace Magnum.Common.Specs
         public Guid CorrelationId { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
-        public int Age { get; set; }
-        public long Height { get; set; }
-        public decimal Amount { get; set; }
+        public int? Age { get; set; }
+        public long? Height { get; set; }
+        public decimal? Amount { get; set; }
+
+        public DateTime? Birthdate { get; set; }
     }
 
     internal class CrazyMesssageMap :
@@ -48,22 +54,37 @@ namespace Magnum.Common.Specs
             Field(x => x.Age);
             Field(x => x.Height);
             Field(x => x.Amount);
+
+            // new things are added with version 2
+
+            Field(x => x.Birthdate, 2);
         }
     }
 
     internal class MessageMap<T>
     {
-        private List<FieldMap> _fields = new List<FieldMap>();
+        private List<FieldMap<T>> _fields = new List<FieldMap<T>>();
 
-        protected FieldMap Field<TField>(Expression<Func<T, TField>> expression)
+        protected FieldMap<T> Field<TField>(Expression<Func<T, TField>> expression, int version)
         {
             MemberExpression memberExpression = GetMemberExpression(expression);
 
-            FieldMap fieldMap = new FieldMap(memberExpression);
+            FieldMap<T> fieldMap = new FieldMap<T>(memberExpression, version);
 
             _fields.Add(fieldMap);
 
             return fieldMap;
+        }
+
+        /// <summary>
+        /// Maps a field as part of the message body and defaults to version 1
+        /// </summary>
+        /// <typeparam name="TField"></typeparam>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        protected FieldMap<T> Field<TField>(Expression<Func<T, TField>> expression)
+        {
+            return Field(expression, 1);
         }
 
         private static MemberExpression GetMemberExpression<TX, VX>(Expression<Func<TX, VX>> expression)
@@ -84,22 +105,81 @@ namespace Magnum.Common.Specs
 
             return memberExpression;
         }
+
+        public void Validate()
+        {
+            foreach (FieldMap<T> field in _fields)
+            {
+                field.Validate();
+            }
+        }
     }
 
-    internal class FieldMap
+    internal class FieldMap<T>
     {
         private readonly MemberExpression _expression;
         private bool _required;
 
-        public FieldMap(MemberExpression expression)
+        public FieldMap(MemberExpression expression, int version)
         {
             _expression = expression;
         }
 
-        public FieldMap Required()
+        public FieldMap<T> Required()
         {
             _required = true;
             return this;
+        }
+
+        public void Validate()
+        {
+            Type propertyType = ((PropertyInfo) _expression.Member).PropertyType;
+
+            if (propertyType.IsValueType)
+            {
+                if (_required == false)
+                {
+                    if (!propertyType.IsGenericType || propertyType.GetGenericTypeDefinition() != typeof (Nullable<>))
+                    {
+                        throw new FieldValidationException(propertyType, "Value types must be nullable if not required: " + _expression.Member.Name);
+                    }
+                }
+            }
+        }
+    }
+
+    internal class FieldValidationException : Exception
+    {
+        private readonly Type _type;
+
+        public FieldValidationException(Type type, string message)
+            : base(message)
+        {
+            _type = type;
+        }
+
+        public FieldValidationException()
+        {
+        }
+
+        public FieldValidationException(string message)
+            : base(message)
+        {
+        }
+
+        public FieldValidationException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected FieldValidationException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        public Type Type
+        {
+            get { return _type; }
         }
     }
 }
