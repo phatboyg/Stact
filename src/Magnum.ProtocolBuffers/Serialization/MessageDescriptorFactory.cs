3618981ccf096d1c1a83ba96e807d92abb6bfaed
Dru Sellers <dru@drusellers.com>
2008-11-13 01:37:13 +0000

    stupid set method

--- src/Magnum.ProtocolBuffers/Serialization/MessageDescriptorFactory.cs
+++ src/Magnum.ProtocolBuffers/Serialization/MessageDescriptorFactory.cs
@@ -2,6 +2,8 @@ namespace Magnum.ProtocolBuffers.Serialization
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq.Expressions;
+    using System.Reflection;
 
     public class MessageDescriptorFactory
     {
@@ -18,8 +20,8 @@ namespace Magnum.ProtocolBuffers.Serialization
             {
                 var wireType = DetermineWireType(field.FieldType);
                 var tag = field.NumberTag;
-                var readFunc = field.Lambda.Compile();
-                var writeFunc = field.Lambda.Compile();
+                var readFunc = GetReader(field.Lambda);
+                var writeFunc = GetWriter(field.Lambda);
 
                 desc.AddWriter(tag, wireType, writeFunc);
                 desc.AddReader(tag, wireType, readFunc);
@@ -32,6 +34,48 @@ namespace Magnum.ProtocolBuffers.Serialization
         private WireType DetermineWireType(Type type)
         {
             return WireType.Varint;
+        }
+
+        private Func<T, TProperty> GetReader<T, TProperty>(Expression<Func<T, TProperty>> expression)
+        {
+            var body = expression.Body as MemberExpression;
+
+            if (body.Member.MemberType != MemberTypes.Property)
+                throw new ArgumentException("Not a property: " + body.Member.Name);
+
+            Type t = body.Member.DeclaringType;
+            PropertyInfo prop = t.GetProperty(body.Member.Name,
+                                              BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            ParameterExpression instance = Expression.Parameter(typeof(T), "instance");
+            var property =
+                Expression.Lambda<Func<T, TProperty>>(Expression.Call(instance, prop.GetGetMethod()), instance)
+                    .Compile();
+
+
+            return property;
+        }
+
+        private Action<T, TProperty> GetWriter<T, TProperty>(Expression<Func<T, TProperty>> expression)
+        {
+            var body = expression.Body as MemberExpression;
+
+            if (body.Member.MemberType != MemberTypes.Property)
+                throw new ArgumentException("Not a property: " + body.Member.Name);
+
+            Type t = body.Member.DeclaringType;
+            PropertyInfo prop = t.GetProperty(body.Member.Name,
+                                              BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            ParameterExpression instance = Expression.Parameter(typeof(T), "instance");
+            var property =
+                Expression.Lambda<Action<T, TProperty>>(Expression.Call(instance, prop.GetSetMethod()), instance)
+                    .Compile();
+
+            
+
+
+            return property;
         }
     }
 }
