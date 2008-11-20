16ba638d2bd7f8e0784ac1f89c6ea0637ab8e2fe
Dru Sellers <dru@drusellers.com>
2008-11-20 02:23:58 +0000

    whoot lambda's for the win baby

--- src/Magnum.ProtocolBuffers/CommunicationModel.cs
+++ src/Magnum.ProtocolBuffers/CommunicationModel.cs
@@ -3,6 +3,7 @@ namespace Magnum.ProtocolBuffers
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq.Expressions;
     using System.Reflection;
     using Internal;
     using Serialization;
@@ -12,6 +13,7 @@ namespace Magnum.ProtocolBuffers
         private readonly IDictionary<Type, IMap> _mappings = new Dictionary<Type, IMap>();
         private readonly MessageDescriptorFactory _factory = new MessageDescriptorFactory();
         private readonly List<IMessageDescriptor> _descriptors = new List<IMessageDescriptor>();
+
 
         public int NumberOfMessagesMapped
         {
@@ -24,11 +26,34 @@ namespace Magnum.ProtocolBuffers
             {
                 if(!type.IsGenericType && typeof(IMap).IsAssignableFrom(type))
                 {
-                    var map = (IMap)Activator.CreateInstance(type);
-                    AddMapping(map);
+                    Type messageType = type.BaseType.GetGenericArguments()[0];
+                    var arg = Expression.Parameter(type.GetInterfaces()[0], "map");
+                    var call = Expression.Call(Expression.Parameter(typeof (CommunicationModel), "who_cares"),
+                                               "AddMapping", new[] {messageType}, arg);
+
+                    var map = Activator.CreateInstance(type);
+                    call.Method.Invoke(this, new []{map});
                 }
             }
         }
+
+
+        public void AddMapping<TMessage>(IMap<TMessage> map) where TMessage : class, new()
+        {
+            if(_mappings.ContainsKey(map.TypeMapped))
+                throw new ProtoMappingException(string.Format("You have already added the type {0} to the communication model", map.TypeMapped));
+
+            _mappings.Add(map.TypeMapped, map);
+            _descriptors.Add(_factory.Build(map));
+        }
+
+
+
+
+
+
+
+
 
         public void WriteMappingsTo(string directory)
         {
@@ -38,22 +63,6 @@ namespace Magnum.ProtocolBuffers
 
         }
 
-        private void AddMapping(IMap map)
-        {
-            if (_mappings.ContainsKey(map.TypeMapped))
-                throw new ProtoMappingException(string.Format("You have already added the type {0} to the communication model", map.TypeMapped));
-
-            _mappings.Add(map.TypeMapped, map);
-            //_descriptors.Add(_factory.Build(map));
-        }
-        private void AddMapping<TMessage>(IMap<TMessage> map) where TMessage : class, new()
-        {
-            if(_mappings.ContainsKey(map.TypeMapped))
-                throw new ProtoMappingException(string.Format("You have already added the type {0} to the communication model", map.TypeMapped));
-
-            _mappings.Add(map.TypeMapped, map);
-            _descriptors.Add(_factory.Build(map));
-        }
 
         public void Validate()
         {
