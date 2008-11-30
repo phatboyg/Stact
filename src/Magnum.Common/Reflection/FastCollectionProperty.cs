472427202b156edb6e81f80ba2c383b1a836aeca
Dru Sellers <dru@drusellers.com>
2008-11-30 03:29:29 +0000

    starting the fastpropertycollection class

---
+++ src/Magnum.Common/Reflection/FastCollectionProperty.cs
@@ -0,0 +1,22 @@
+namespace Magnum.Common.Reflection
+{
+    public class FastCollectionProperty
+    {
+        
+    }
+
+    public class FastCollectionProperty<TClass>
+    {
+
+    }
+
+    public class FastCollectionProperty<TClass, TCollection>
+    {
+
+    }
+
+    public class FastCollectionProperty<TClass, TCollection, TElement>
+    {
+
+    }
+}
