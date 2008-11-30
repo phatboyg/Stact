f8f1dcf72203e58cc30cd7a9a825f6e134d0f84e
Dru Sellers <dru@drusellers.com>
2008-11-30 03:47:18 +0000

    ao

--- src/Magnum.Common/Reflection/FastCollectionProperty.cs
+++ src/Magnum.Common/Reflection/FastCollectionProperty.cs
@@ -1,22 +1,41 @@
 namespace Magnum.Common.Reflection
 {
+    using System.Collections;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Represents a property that has a collection type that needs to be manipulated
+    /// </summary>
     public class FastCollectionProperty
     {
-        
+        private FastCollection fc;
+        private FastProperty fp;
     }
 
+    /// <summary>
+    /// Represents a property that has a collection type that needs to be manipulated
+    /// </summary>
     public class FastCollectionProperty<TClass>
     {
-
+        private FastCollection fc;
+        private FastProperty<TClass> fp;
     }
 
-    public class FastCollectionProperty<TClass, TCollection>
+    /// <summary>
+    /// Represents a property that has a collection type that needs to be manipulated
+    /// </summary>
+    public class FastCollectionProperty<TClass, TCollection> where TCollection : IList
     {
-
+        private FastCollection<TCollection> fc;
+        private FastProperty<TClass, TCollection> fp;
     }
 
-    public class FastCollectionProperty<TClass, TCollection, TElement>
+    /// <summary>
+    /// Represents a property that has a collection type that needs to be manipulated
+    /// </summary>
+    public class FastCollectionProperty<TClass, TCollection, TElement> where TCollection : ICollection<TElement>
     {
-
+        private FastCollection<TCollection, TElement> fc;
+        private FastProperty<TClass, TCollection> fp;
     }
 }
