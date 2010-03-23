f8654ec2f7f7eb61a47aadc5498b8727a4f24d06
Chris Patterson <chris@phatboyg.com>
2010-03-22 21:21:25 -0500

    Adding channel API the right way

---
+++ src/Magnum.Specs/Channels/ChannelQueue_Specs.cs
@@ -0,0 +1,84 @@
+// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace Magnum.Specs.Channels
+{
+	using DateTimeExtensions;
+	using Magnum.Actions;
+	using Magnum.Actors;
+	using Magnum.Channels;
+	using NUnit.Framework;
+	using TestFramework;
+
+	[TestFixture]
+	public class Subscribing_to_a_channel
+	{
+		[Test]
+		public void Should_place_the_consumer_action_on_the_queue()
+		{
+			var actor = new SomeActorInstance();
+
+			var message = new MyMessage();
+
+			actor.MessageChannel.Send(message);
+
+			actor.Future.IsAvailable(1.Seconds()).ShouldBeTrue();
+			actor.Future.Value.ShouldEqual(message);
+		}
+
+		[Test]
+		public void Should_place_the_consumer_action_on_the_queue_for_lambdas()
+		{
+			var actor = new SomeActorInstance();
+
+			var message = new MyMessage();
+
+			actor.LambdaMessageChannel.Send(message);
+
+			actor.Future.IsAvailable(1.Seconds()).ShouldBeTrue();
+			actor.Future.Value.ShouldEqual(message);
+		}
+	}
+
+
+	public class SomeActorInstance
+	{
+		private readonly Future<MyMessage> _future;
+		private readonly ThreadPoolActionQueue _queue;
+
+		public SomeActorInstance()
+		{
+			_queue = new ThreadPoolActionQueue();
+			_future = new Future<MyMessage>();
+
+			MessageChannel = new ActionQueueChannel<MyMessage>(_queue, Consume);
+			LambdaMessageChannel = new ActionQueueChannel<MyMessage>(_queue, message => _future.Complete(message));
+		}
+
+		public Future<MyMessage> Future
+		{
+			get { return _future; }
+		}
+
+		public Channel<MyMessage> MessageChannel { get; private set; }
+		public Channel<MyMessage> LambdaMessageChannel { get; private set; }
+
+		private void Consume(MyMessage message)
+		{
+			_future.Complete(message);
+		}
+	}
+
+	public class MyMessage
+	{
+	}
+}
