5e1c65a20edd466daa5d3bfd274ed650e672220b
Chris Patterson <chris@phatboyg.com>
2010-06-28 17:00:06 -0500

    Another test on value provider with Json

--- src/Magnum.Specs/Configuration/ConfigurationBinder_Specs.cs
+++ src/Magnum.Specs/Configuration/ConfigurationBinder_Specs.cs
@@ -12,29 +12,39 @@
 // specific language governing permissions and limitations under the License.
 namespace Magnum.Specs.Configuration
 {
+	using System.Collections.Generic;
+	using System.Diagnostics;
+	using System.IO;
+	using System.Text;
 	using Magnum.Configuration;
 	using Magnum.ValueProviders;
 	using TestFramework;
 	using ValueProviders;
 
 	[Scenario]
-	public class When_a_configuration_object_is_bound :
-		Given_a_command_line
+	public class When_a_configuration_object_is_bound 
 	{
 		ConfigurationBinder _configurationBinder;
 		MyConfiguration _configuration;
+		MultipleValueProvider _provider;
 
 		[When]
 		public void A_configuration_object_is_bound()
 		{
+			const string json = @"{ Name: ""phatboyg"", Password: ""default""}";
+			var jsonStream = new MemoryStream(Encoding.UTF8.GetBytes(json));
+
+			string commandLineText = "-password:really_long_one --secure";
+
 			ValueProvider[] providers = new ValueProvider[]
 				{
-					new Magnum.ValueProviders.CommandLineValueProvider(CommandLineText)
+					new Magnum.ValueProviders.CommandLineValueProvider(commandLineText),
+					new Magnum.ValueProviders.JsonValueProvider(jsonStream),
 				};
 
-			var provider = new MultipleValueProvider(providers);
+			_provider = new MultipleValueProvider(providers);
 
-			_configurationBinder = new ConfigurationBinder(provider);
+			_configurationBinder = new ConfigurationBinder(_provider);
 
 			_configuration = _configurationBinder.Bind<MyConfiguration>();
 		}
@@ -55,6 +65,18 @@ namespace Magnum.Specs.Configuration
 		public void Should_have_secure_value()
 		{
 			_configuration.Secure.ShouldBeTrue();
+		}
+
+		[Then]
+		public void Should_return_a_list_of_all_configuration()
+		{
+			var found = new Dictionary<string,object>();
+			_provider.GetAll(found.Add);
+
+			found.ContainsKey("password").ShouldBeTrue();
+			found.ContainsKey("secure").ShouldBeTrue();
+			found.ContainsKey("Name").ShouldBeTrue();
+			found.ContainsKey("Password").ShouldBeTrue();
 		}
 
 
--- src/Magnum/ValueProviders/JsonValueProvider.cs
+++ src/Magnum/ValueProviders/JsonValueProvider.cs
@@ -10,7 +10,7 @@
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
-namespace Magnum.Web.ValueProviders
+namespace Magnum.ValueProviders
 {
 	using System;
 	using System.Collections.Generic;
@@ -18,7 +18,6 @@ namespace Magnum.Web.ValueProviders
 	using System.IO;
 	using System.Web.Script.Serialization;
 	using Extensions;
-	using Magnum.ValueProviders;
 
 
 	public class JsonValueProvider :
