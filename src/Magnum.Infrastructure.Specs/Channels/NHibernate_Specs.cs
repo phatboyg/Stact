b2570ad49e5debfacb69d622a6bfd66e2c15065f
Chris Patterson <chris@phatboyg.com>
2010-07-08 19:46:17 -0500

    Added test for NHibernate channel and it works

---
+++ src/Magnum.Infrastructure.Specs/Channels/NHibernate_Specs.cs
@@ -0,0 +1,58 @@
+// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace Magnum.Infrastructure.Specs.Channels
+{
+	using FluentNHibernate.Cfg;
+	using NHibernate;
+	using NHibernate.Tool.hbm2ddl;
+	using TestFramework;
+
+
+	[Scenario]
+	public class Given_an_nhibernate_session_factory
+	{
+		static ISessionFactory _sessionFactory;
+
+		public ISessionFactory SessionFactory
+		{
+			get
+			{
+				if (_sessionFactory == null)
+					_sessionFactory = CreateSessionFactory();
+
+				return _sessionFactory;
+			}
+		}
+
+		static ISessionFactory CreateSessionFactory()
+		{
+			return Fluently.Configure()
+				.Mappings(m => m.FluentMappings.AddFromAssemblyOf<Given_an_nhibernate_session_factory>())
+				.ExposeConfiguration(cfg =>
+					{
+						new SchemaExport(cfg).Create(false, true);
+
+
+//					cfg.EventListeners.PreInsertEventListeners =
+//						new IPreInsertEventListener[] { context.GetInstance<AuditEventListener>() };
+//
+//					cfg.EventListeners.PreUpdateEventListeners =
+//						new IPreUpdateEventListener[] { context.GetInstance<AuditEventListener>() };
+//
+//					cfg.EventListeners.PreDeleteEventListeners =
+//						new IPreDeleteEventListener[] { context.GetInstance<AuditEventListener>() };
+					})
+				.BuildSessionFactory();
+		}
+	}
+}
---
+++ src/Magnum.Infrastructure.Specs/Channels/TestInstance.cs
@@ -0,0 +1,75 @@
+﻿// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+namespace Magnum.Infrastructure.Specs.Channels
+{
+	using Fibers;
+	using Magnum.Channels;
+
+
+	public class TestInstance
+	{
+		public TestInstance(int id)
+			: this()
+		{
+			Id = id;
+		}
+
+		protected TestInstance()
+		{
+			UpdateValueChannel = new ConsumerChannel<UpdateValue>(new SynchronousFiber(), m => Value = m.Value);
+		}
+
+		public virtual Channel<UpdateValue> UpdateValueChannel { get; private set; }
+
+		public virtual int Id { get; private set; }
+		public virtual string Value { get; set; }
+
+		public virtual bool Equals(TestInstance other)
+		{
+			if (ReferenceEquals(null, other))
+				return false;
+			if (ReferenceEquals(this, other))
+				return true;
+			return other.Id == Id;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj))
+				return false;
+			if (ReferenceEquals(this, obj))
+				return true;
+			if (obj.GetType() != typeof(TestInstance))
+				return false;
+			return Equals((TestInstance)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return Id;
+		}
+	}
+
+
+	public class UpdateValue
+	{
+		public UpdateValue(int id, string value)
+		{
+			Id = id;
+			Value = value;
+		}
+
+		public int Id { get; private set; }
+		public string Value { get; private set; }
+	}
+}
