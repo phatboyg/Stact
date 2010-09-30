d3f61177fcc6cbe42d562caad50987de1f567f6a
Chris Patterson <chris@phatboyg.com>
2010-09-30 11:44:32 -0500

    Building with references to Magnum

--- src/Stact/Actor.cs
+++ src/Stact/Actor.cs
@@ -1,4 +1,4 @@
-// Copyright 2007-2010 The Apache Software Foundation.
+// Copyright 2010 Chris Patterson
 //  
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 // this file except in compliance with the License. You may obtain a copy of the 
@@ -10,7 +10,7 @@
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
-namespace Stact.Actors
+namespace Stact
 {
 	/// <summary>
 	/// Defines a role of actor for a class
